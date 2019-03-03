7b5ddbac1db3375cf2a3d61facb1cf41ccd96450
rio.threehouse <threehouse.second@gmail.com>
2019-03-03 08:26:13 +0000

    modified tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :corrent_user, only: [:update, :destroy]
+  before_action :corrent_user, only: [:update, :destroy, :show, :edit]
   
   def show
   end
